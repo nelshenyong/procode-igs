6cac219ba524a1205d272071ddaceaee8c41de46
nelshenyong <nelshenyong@gmail.com>
2024-10-17 15:51:53 +0700

    update

--- REACTJS/MEET06/split-bill/src/App.jsx
+++ REACTJS/MEET06/split-bill/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react';
-import friendsList from './friends.json';
-import FormSplitBill from './components/FormSplitBill';
-import FriendListCard from './components/FriendListCard';
+import { useState } from "react";
+import friendsList from "./friends.json"
+
+import FormSplitBill from "./components/FormSplitBill";
+import FriendListCard from "./components/FriendListCard";
 
 function App() {
   const [friends, setFriends] = useState(friendsList);
 
   return (
     <>
-      <div className="app">
-        <div className="sidebar">
-          <FriendListCard friends={friends} />
-        </div>
-      <FormSplitBill />
+     <div className="app">
+      <div className="sidebar">
+        <FriendListCard
+          friends={friends}
+        />
       </div>
-
+      <FormSplitBill />
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
